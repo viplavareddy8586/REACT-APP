7e1232404a22b01f8d5aae5a36b4396a8c02369c
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-11-15 12:49:37 +0530

    Add files via upload

---
+++ Nov15-Components/first.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class First extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>Welcome to React </h1>
+        );
+    }
+}
+ 
+export default First;
