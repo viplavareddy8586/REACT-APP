0309ca14f68452956bc0a0764a1b5a9d5078a792
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-11-15 14:48:50 +0530

    Add files via upload

---
+++ Nov15-example-2components/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Sample from './Nov15-example-2components/sample';
+import Second from './Nov15-example-2components/second';
+
+function App() {
+  return (
+    <div>
+      <Sample/>
+    <Second/>
+    </div>
+  );
+}
+
+export default App;
