f0c4accc2f871b4a17df6815773f37fb2aa50c7f
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-11-22 12:03:00 +0530

    Add files via upload

---
+++ nov22firstcomponents/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import First from './mycomponentsNov21/first';
+import Second from './mycomponentsNov21/second';
+
+function App() {
+  return (
+    <div>
+      <First/>
+    <Second/>
+    </div>
+  );
+}
+
+export default App;
