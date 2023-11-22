6a4d7eec2d07766c2231cbccc8b1ee94f8da07cf
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-11-22 15:06:53 +0530

    Update App.js

--- nov22firstcomponents/App.js
+++ nov22firstcomponents/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import First from './mycomponentsNov21/first';
 import Second from './mycomponentsNov21/second';
 import Thirdtable from './nov22firstcomponents/thirdtable';
-
+import Fourth from './nov22firstcomponents/fourth';
 function App() {
   return (
     <div>
       <First/>
     <Second/>
     <Thirdtable/>
+    <Fourth/>
     </div>
   );
 }
