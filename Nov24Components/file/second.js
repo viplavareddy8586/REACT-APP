d6f3458cd96d0ecd6003fc159a41e8ee23e1c349
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-11-24 16:40:42 +0530

    Add files via upload

---
+++ Nov24Components/file/second.js
@@ -0,0 +1,48 @@
+const student=[
+    {
+        "studentname":"viplav",
+         "mathsmarks":45,
+         "sciencemarks":38,
+         "socialmarks":60
+    },
+    {
+        "studentname":"rohith",
+         "mathsmarks":25,
+         "sciencemarks":50,
+         "socialmarks":30
+    },
+    {
+        "studentname":"shiva",
+         "mathsmarks":36,
+         "sciencemarks":10,
+         "socialmarks":40
+    },
+    {
+        "studentname":"charan",
+         "mathsmarks":5,
+         "sciencemarks":20,
+         "socialmarks":50
+    },
+    {
+        "studentname":"anand",
+         "mathsmarks":75,
+         "sciencemarks":60,
+         "socialmarks":90
+    },
+    {
+        "studentname":"bharath",
+         "mathsmarks":45,
+         "sciencemarks":20,
+         "socialmarks":40
+    },
+    {
+        "studentname":"srinadh",
+         "mathsmarks":55,
+         "sciencemarks":60,
+         "socialmarks":50
+    },
+
+]
+export function getstudent(){
+    return student;
+}
