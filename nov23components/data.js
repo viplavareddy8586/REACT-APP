a54d010a3a101e795c1495581443f8ed10ac8c57
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-11-24 11:38:02 +0530

    Add files via upload

---
+++ nov23components/data.js
@@ -0,0 +1,33 @@
+ const emps=[
+    {
+        "id": 101,
+        "name": "Abdul",
+        "dept": "IT",
+        "salary":45000
+    },
+    {
+        "id": 102,
+        "name": "John",
+        "dept": "Sales",
+        "salary":25000
+    },
+    {
+        "id": 103,
+        "name": "Ramu",
+        "dept": "Admin",
+        "salary":15000
+    },
+    {
+        "id": 104,
+        "name": "Viplav",
+        "dept": "Bank",
+        "salary":50000
+    },
+    {
+        "id": 105,
+        "name": "Rohith",
+        "dept": "Insurance",
+        "salary":15000
+    }
+]
+export default emps;
