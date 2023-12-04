8f70bdc68809314767889565a287045e5ce00df7
viplavareddy8586 <146053562+viplavareddy8586@users.noreply.github.com>
2023-12-04 15:11:11 +0530

    Add files via upload

---
+++ dec4components/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import {Route} from 'react-router-dom';
+import Home from './dec4components/home';
+import About from './dec4components/about';
+import Contact from './dec4components/contact';
+
+import UnoNavbar from './dec4components/unonavbar';
+import Loginform from './dec4components/loginform';
+
+function App() {
+  return (
+    <div>
+      <UnoNavbar/>
+      <div>
+        <Route path='/home' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/contact' component={Contact}/>
+        <Route path='/' component={Loginform}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
